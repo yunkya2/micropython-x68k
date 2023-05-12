b1588c989bdb33ddf0268dbb2b62cc35d4e3228e
Yuichi Nakamura <y.512.nakamura@gmail.com>
2023-05-13 00:44:58 +0900

    Refactor x68k config file for v1.20.0 update

--- ports/x68k/mpconfigport.h
+++ ports/x68k/mpconfigport.h
@@ -28,100 +28,156 @@
 
 #include <stdint.h>
 
-// Board and hardware specific configuration
-#define MICROPY_HW_MCU_NAME                     "m68000"
-#define MICROPY_HW_BOARD_NAME                   "x68k"
-#define MICROPY_PY_SYS_PLATFORM                 "human68k"
-
+// Set base feature level.
 #define MICROPY_CONFIG_ROM_LEVEL (MICROPY_CONFIG_ROM_LEVEL_EXTRA_FEATURES)
+
+// Board and hardware specific configuration
+#define MICROPY_HW_MCU_NAME            "m68000"
+#define MICROPY_HW_BOARD_NAME          "x68k"
+#define MICROPY_PY_SYS_PLATFORM        "human68k"
+
+// Enable helpers for printing debugging information.
+#ifndef MICROPY_DEBUG_PRINTERS
+#define MICROPY_DEBUG_PRINTERS         (1)
+#endif
+
+// Enable floating point by default.
+#ifndef MICROPY_FLOAT_IMPL
+#define MICROPY_FLOAT_IMPL             (MICROPY_FLOAT_IMPL_FLOAT)
+#endif
+
+// Enable arbritrary precision long-int by default.
+#ifndef MICROPY_LONGINT_IMPL
+#define MICROPY_LONGINT_IMPL           (MICROPY_LONGINT_IMPL_MPZ)
+#endif
+
+// REPL conveniences.
+#define MICROPY_REPL_EMACS_WORDS_MOVE  (1)
+#define MICROPY_REPL_EMACS_EXTRA_WORDS_MOVE (1)
+#define MICROPY_USE_READLINE_HISTORY   (1)
+#ifndef MICROPY_READLINE_HISTORY_SIZE
+#define MICROPY_READLINE_HISTORY_SIZE  (50)
+#endif
+
+// Allow exception details in low-memory conditions.
+#define MICROPY_ENABLE_EMERGENCY_EXCEPTION_BUF (1)
+#define MICROPY_EMERGENCY_EXCEPTION_BUF_SIZE (256)
+
+// Allow loading of .mpy files.
+#define MICROPY_PERSISTENT_CODE_LOAD   (1)
+
+// Enable a small performance boost for the VM.
+#define MICROPY_OPT_COMPUTED_GOTO      (1)
+
+// Enable detailed error messages.
+#define MICROPY_ERROR_REPORTING     (MICROPY_ERROR_REPORTING_DETAILED)
+
+// Configure the "sys" module with features not usually enabled on bare-metal.
+#define MICROPY_PY_SYS_ATEXIT          (1)
+
+// Configure the "os" module with extra unix features.
+#define MICROPY_PY_UOS_INCLUDEFILE     "ports/x68k/moduos.c"
+#define MICROPY_PY_UOS_ERRNO           (1)
+#define MICROPY_PY_UOS_GETENV_PUTENV_UNSETENV (1)
+#define MICROPY_PY_UOS_SEP             (1)
+#define MICROPY_PY_UOS_SYSTEM          (1)
+#define MICROPY_PY_UOS_UNAME           (1)
+#define MICROPY_PY_UOS_STATVFS         (1)
+#define MICROPY_PY_UOS_URANDOM         (0)
+#define MICROPY_PY_UOS_SYNC            (0)
+
+// Enable the unix-specific "time" module.
+#define MICROPY_PY_UTIME               (1)
+#define MICROPY_PY_UTIME_MP_HAL        (1)
+
+// Disable the "select" module.
+#define MICROPY_PY_USELECT             (0)
+
+// Enable the "machine" module.
+#define MICROPY_PY_MACHINE             (1)
+#define MICROPY_PY_MACHINE_PIN_MAKE_NEW     mp_pin_make_new
+
+#ifndef MICROPY_PY_SYS_PATH_DEFAULT
+#define MICROPY_PY_SYS_PATH_DEFAULT ".frozen"
+#endif
+
+#define MP_STATE_PORT MP_STATE_VM
+
+// Configure which emitter to use for this target.
+#define MICROPY_EMIT_INLINE_M68K    (1)
+#define MICROPY_EMIT_M68K           (1)
+
+// Type definitions for the specific machine based on the word size.
+typedef intptr_t mp_int_t; // must be pointer size
+typedef uintptr_t mp_uint_t; // must be pointer size
+
+// Cannot include <sys/types.h>, as it may lead to symbol name clashes
+typedef long mp_off_t;
 
 #define MICROPY_OBJ_BASE_ALIGNMENT  __attribute__((aligned(4)))
 
+// We need to provide a declaration/definition of alloca()
+#include <alloca.h>
+
+// Always enable GC.
+#define MICROPY_ENABLE_GC           (1)
+
+#if !(defined(MICROPY_GCREGS_SETJMP))
+// Fall back to setjmp() implementation for discovery of GC pointers in registers.
+#define MICROPY_GCREGS_SETJMP (1)
+#endif
+
+// Enable the VFS
+#define MICROPY_ENABLE_FINALISER    (1)
+#define MICROPY_VFS                 (1)
+#define MICROPY_READER_VFS          (1)
+#define MICROPY_VFS_POSIX           (0)
+
+// VFS stat functions should return time values relative to 1970/1/1
+#define MICROPY_EPOCH_IS_1970       (1)
+
+// Disable stackless by default.
+#ifndef MICROPY_STACKLESS
+#define MICROPY_STACKLESS           (0)
+#define MICROPY_STACKLESS_STRICT    (0)
+#endif
+
+// Ensure builtinimport.c works with -m.
+#define MICROPY_MODULE_OVERRIDE_MAIN_IMPORT (1)
+
+// Don't default sys.argv because we do that in main.
+#define MICROPY_PY_SYS_PATH_ARGV_DEFAULTS (0)
+
+// Bare-metal ports don't have stderr. Printing debug to stderr may give tests
+// which check stdout a chance to pass, etc.
+extern const struct _mp_print_t mp_stderr_print;
+#define MICROPY_DEBUG_PRINTER (&mp_stderr_print)
+#define MICROPY_ERROR_PRINTER (&mp_stderr_print)
+
+#ifndef MICROPY_EVENT_POLL_HOOK
+#define MICROPY_EVENT_POLL_HOOK \
+    do { \
+        extern void mp_handle_pending(bool); \
+        mp_handle_pending(true); \
+    } while (0);
+#endif
+
 // Python internal features.
-#define MICROPY_PERSISTENT_CODE_LOAD            (1)
-#define MICROPY_EMIT_INLINE_M68K                (1)
-#define MICROPY_EMIT_M68K                       (1)
 #define MICROPY_SMALL_INT_MUL_HELPER            (1)
-#define MICROPY_ENABLE_GC                       (1)
-#define MICROPY_GCREGS_SETJMP                   (1)
-#define MICROPY_HELPER_REPL                     (1)
-#define MICROPY_ERROR_REPORTING                 (MICROPY_ERROR_REPORTING_TERSE)
-#define MICROPY_FLOAT_IMPL                      (MICROPY_FLOAT_IMPL_FLOAT)
-#define MICROPY_LONGINT_IMPL                    (MICROPY_LONGINT_IMPL_MPZ)
-#define MICROPY_SCHEDULER_DEPTH                 (8)
+
 #define MICROPY_SCHEDULER_STATIC_NODES          (1)
-#define MICROPY_ENABLE_EMERGENCY_EXCEPTION_BUF  (1)
-#define MICROPY_EMERGENCY_EXCEPTION_BUF_SIZE    (0)
 
-#define MICROPY_MODULE_WEAK_LINKS               (1)
-#define MICROPY_CAN_OVERRIDE_BUILTINS           (0)
-#define MICROPY_PY_FUNCTION_ATTRS               (1)
-
+#define MICROPY_PY_USELECT                      (0)
 #define MICROPY_PY_ASYNC_AWAIT                  (0)
-#define MICROPY_PY_BUILTINS_SET                 (0)
+#define MICROPY_PY_UASYNCIO                     (0)
+
 #define MICROPY_PY_BUILTINS_STR_UNICODE         (0)
 #define MICROPY_PY_BUILTINS_STR_SJIS            (1)
 #define MICROPY_PY_BUILTINS_STR_SJIS_CHECK      (1)
-#define MICROPY_PY_ATTRTUPLE                    (1)
-#define MICROPY_PY_COLLECTIONS                  (0)
-#define MICROPY_PY_MATH                         (1)
-#define MICROPY_PY_IO                           (1)
-#define MICROPY_PY_STRUCT                       (1)
-#define MICROPY_PY_SYS                          (1)
-#define MICROPY_PY_SYS_PATH_DEFAULT             ".frozen"
-#define MICROPY_PY_MACHINE                      (1)
-#define MICROPY_PY_MACHINE_PIN_MAKE_NEW         mp_pin_make_new
-#define MICROPY_PY_UTIME                        (1)
-#define MICROPY_PY_UTIME_MP_HAL                 (1)
-#define MICROPY_READER_VFS                      (1)
-#define MICROPY_VFS                             (1)
-#define MICROPY_VFS_POSIX                       (0)
-
-#define mp_type_fileio mp_type_vfs_human_fileio
-#define mp_type_textio mp_type_vfs_human_textio
-
-#define MICROPY_PY_UOS                          (1)
-#define MICROPY_PY_UOS_INCLUDEFILE              "ports/x68k/moduos.c"
-#define MICROPY_PY_UOS_GETENV_PUTENV_UNSETENV   (1)
-#define MICROPY_PY_UOS_SEP                      (1)
-#define MICROPY_PY_UOS_SYNC                     (0)
-#define MICROPY_PY_UOS_SYSTEM                   (1)
-#define MICROPY_PY_UOS_UNAME                    (1)
-#define MICROPY_PY_UOS_URANDOM                  (0)
-#define MICROPY_PY_UOS_ERRNO                    (1)
-#define MICROPY_PY_UOS_STATVFS                  (1)
-
-#define MICROPY_PY_UJSON                        (1)
-#define MICROPY_PY_USELECT                      (0)
 
 #define MICROPY_PY_URE                          (1)
 #define MICROPY_PY_URE_MATCH_GROUPS             (1)
 #define MICROPY_PY_URE_MATCH_SPAN_START_END     (1)
 #define MICROPY_PY_URE_SUB                      (1)
 
-#define MICROPY_EVENT_POLL_HOOK \
-    do { \
-        extern void mp_handle_pending(bool); \
-        mp_handle_pending(true); \
-    } while (0);
-
-#define MICROPY_REPL_EMACS_KEYS                 (1)
-#define MICROPY_REPL_EMACS_WORDS_MOVE           (1)
-#define MICROPY_REPL_EMACS_EXTRA_WORDS_MOVE     (1)
-#define MICROPY_USE_READLINE_HISTORY            (1)
-
-#define MICROPY_PORT_ROOT_POINTERS                                        \
-    const char *readline_hist[50];                                        \
-
-#define MP_STATE_PORT MP_STATE_VM
-
-// Type definitions for the specific machine.
-
-typedef intptr_t mp_int_t; // must be pointer size
-typedef uintptr_t mp_uint_t; // must be pointer size
-typedef long mp_off_t;
-
 #define MP_SSIZE_MAX (0x7fffffff)
-
-// We need to provide a declaration/definition of alloca()
-#include <alloca.h>
