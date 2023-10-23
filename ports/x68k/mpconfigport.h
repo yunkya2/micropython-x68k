00e112b519fb40ae44660154c22f982f4f6d52b5
Yuichi Nakamura <y.512.nakamura@gmail.com>
2023-10-23 23:07:39 +0900

    all: Fix spelling mistakes based on codespell check.

--- ports/x68k/mpconfigport.h
+++ ports/x68k/mpconfigport.h
@@ -46,7 +46,7 @@
 #define MICROPY_FLOAT_IMPL             (MICROPY_FLOAT_IMPL_FLOAT)
 #endif
 
-// Enable arbritrary precision long-int by default.
+// Enable arbitrary precision long-int by default.
 #ifndef MICROPY_LONGINT_IMPL
 #define MICROPY_LONGINT_IMPL           (MICROPY_LONGINT_IMPL_MPZ)
 #endif
