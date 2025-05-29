7ba8b5f65436479f776fbc8f257a5337d8756596
Yuichi Nakamura <y.512.nakamura@gmail.com>
2025-05-29 22:46:59 +0900

    all: Use mp_obj_malloc_with_finaliser everywhere it's applicable.
    refer: cae690d047900b842beaa7798b5362b5646130af

--- ports/x68k/vfs_human_file.c
+++ ports/x68k/vfs_human_file.c
@@ -57,7 +57,6 @@ static void vfs_human_file_print(const mp_print_t *print, mp_obj_t self_in, mp_p
 }
 
 mp_obj_t mp_vfs_human_file_open(const mp_obj_type_t *type, mp_obj_t file_in, mp_obj_t mode_in) {
-    mp_obj_vfs_human_file_t *o = m_new_obj_with_finaliser(mp_obj_vfs_human_file_t);
     const char *mode_s = mp_obj_str_get_str(mode_in);
 
     int mode_rw = 0, mode_x = 0, mode_bt = 0;
@@ -88,7 +87,8 @@ mp_obj_t mp_vfs_human_file_open(const mp_obj_type_t *type, mp_obj_t file_in, mp_
         }
     }
 
-    o->base.type = type;
+    mp_obj_vfs_human_file_t *o = mp_obj_malloc_with_finaliser(mp_obj_vfs_human_file_t, type);
+    o->fd = -1; // In case open() fails below, initialise this as a "closed" file object.
 
     mp_obj_t fid = file_in;
 
