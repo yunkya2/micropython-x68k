24283a1fc56f1b010955feaae2532d908f5c4c54
Yuichi Nakamura <y.512.nakamura@gmail.com>
2025-05-29 23:48:18 +0900

    py/emitinlinerv32: Add inline assembler support for RV32.
    refer: 268acb714dd79fa5eeeb82c1fca022bc4ea126b7

--- ports/x68k/mpyconv/mpconfigport.h
+++ ports/x68k/mpyconv/mpconfigport.h
@@ -46,6 +46,7 @@
 #define MICROPY_EMIT_INLINE_XTENSA  (1)
 #define MICROPY_EMIT_XTENSAWIN      (1)
 #define MICROPY_EMIT_RV32           (1)
+#define MICROPY_EMIT_INLINE_RV32    (1)
 #define MICROPY_EMIT_M68K           (1)
 #define MICROPY_EMIT_INLINE_M68K    (1)
 #define MICROPY_EMIT_NATIVE_DEBUG   (1)
